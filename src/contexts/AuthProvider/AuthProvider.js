273eb1e113637e5df7e1f04be458afea34c8dedd
Aminul Islam Rocky <mairoky@outlook.com>
2022-10-25 20:02:53 +0600

    create account using email & password

--- src/contexts/AuthProvider/AuthProvider.js
+++ src/contexts/AuthProvider/AuthProvider.js
@@ -1,5 +1,5 @@
-import React, { createContext } from 'react';
-import { getAuth } from 'firebase/auth';
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signOut, updateProfile } from 'firebase/auth';
 import app from '../../firebase/firebase.config';
 
 
@@ -7,8 +7,43 @@ export const AuthContext = createContext();
 const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    // Create user using email and password
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    // User profile update
+    const updateUserProfile = profile => {
+        return updateProfile(auth.currentUser, profile);
+    }
+
+    // Log out
+    const logOut = () => {
+        setLoading(true);
+        return signOut(auth);
+    }
+
+    // Check & set current user to user state
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            console.log('Auth State Change', currentUser);
+            setUser(currentUser);
+            setLoading(false);
+        })
+        return () => {
+            unsubscribe();
+        }
+    }, [])
     const authInfo = {
-        displayName: 'Aamir'
+        createUser,
+        updateUserProfile,
+        user,
+        logOut,
+        loading
     };
     return (
         <AuthContext.Provider value={authInfo}>
--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,16 +1,57 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FaGoogle, FaFacebook, FaImage, FaUser } from "react-icons/fa";
 import { MdOutlineEmail, MdPassword } from "react-icons/md";
+import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
+import toast from 'react-hot-toast';
 
 const Register = () => {
+    const [error, setError] = useState('');
+    // Get Auth Context Data
+    const { createUser, updateUserProfile } = useContext(AuthContext);
+
+    // Handle Register Form Data 
+    const handleRegistration = e => {
+        e.preventDefault();
+        const form = e.target;
+        const name = form.name.value;
+        const photo = form.photo.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        // console.log(name, photo, email, password);
+        createUser(email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                setError('');
+                form.reset();
+                handleProfileUpdate(name, photo);
+                toast.success('Registration successful! Please verify your email address.');
+            })
+            .catch(err => {
+                console.error(err);
+                toast.error(err.message);
+            })
+    }
+
+    // Update user profile
+    const handleProfileUpdate = (name, photo) => {
+        const profile = {
+            displayName: name,
+            photoURL: photo
+        }
+        updateUserProfile(profile)
+            .then(() => { })
+            .catch(err => console.error(err));
+    }
+
     return (
         <div>
             <div className="container">
                 <div className="row">
                     <div className="col-12 col-md-7 m-auto">
                         <div className="card">
-                            <form>
+                            <form onSubmit={handleRegistration}>
                                 <div className="card-body">
                                     <div className="text-center">
                                         <h3>Create Account</h3>
@@ -37,7 +78,7 @@ const Register = () => {
                                         <input type="password" id="password" name="password" className="form-control" required />
                                     </div>
                                     <div className="text-center">
-                                        <button className="btn btn-success login-btn mt-3">Register</button>
+                                        <button type="submit" className="btn btn-success login-btn mt-3">Register</button>
                                         <Link to="/login" className="d-block register-link">Already have an Account?<span> Login Now</span></Link>
                                     </div>
                                     <hr />
