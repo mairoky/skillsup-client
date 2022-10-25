555cf2f820d069f93b4c1e075def988411cec686
Aminul Islam Rocky <mairoky@outlook.com>
2022-10-25 14:30:47 +0600

    basic routes setup

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,26 +2,27 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link, NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
         <header>
             <Navbar collapseOnSelect expand="lg">
                 <Container>
-                    <Navbar.Brand href="#home">Skills<span>UP</span></Navbar.Brand>
+                    <Navbar.Brand as={Link} to="/">Skills<span>UP</span></Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="me-auto">
-                            <Nav.Link href="#features">Home</Nav.Link>
-                            <Nav.Link href="#pricing">Courses</Nav.Link>
-                            <Nav.Link href="#pricing">FAQ</Nav.Link>
-                            <Nav.Link href="#pricing">Blog</Nav.Link>
+                        <Nav className="m-auto">
+                            <Nav.Link as={Link} to="/" >Home</Nav.Link>
+                            <Nav.Link as={NavLink} to="/course" >Courses</Nav.Link>
+                            <Nav.Link as={NavLink} to="/faq" >Faq</Nav.Link>
+                            <Nav.Link as={NavLink} to="/blog" >Blog</Nav.Link>
 
                         </Nav>
                         <Nav>
-                            <Nav.Link>More</Nav.Link>
+                            <Nav.Link>Dark Theme</Nav.Link>
                             <Nav.Link>
-                                Dark Theme
+                                Profile
                             </Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,31 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layout/Main";
+import Home from "../pages/Home/Home";
+import Course from "../pages/Course/Course";
+import Faq from "../pages/Faq/Faq";
+import Blog from "../pages/Blog/Blog";
 
 export const routes = createBrowserRouter([
     {
         path: '/',
-        element: <Main></Main>
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/course',
+                element: <Course></Course>
+            },
+            {
+                path: '/faq',
+                element: <Faq></Faq>
+            },
+            {
+                path: '/blog',
+                element: <Blog></Blog>
+            }
+        ]
     }
 ])
