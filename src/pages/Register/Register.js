5a181b170f2c818b53f9212abdafe9b25dc7d446
Aminul Islam Rocky <mairoky@outlook.com>
2022-10-26 10:59:52 +0600

    login & registration done

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,14 +1,26 @@
 import React, { useContext, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { FaGoogle, FaFacebook, FaImage, FaUser } from "react-icons/fa";
 import { MdOutlineEmail, MdPassword } from "react-icons/md";
 import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
 import toast from 'react-hot-toast';
+import { FacebookAuthProvider, GoogleAuthProvider } from 'firebase/auth';
 
 const Register = () => {
+
     const [error, setError] = useState('');
     // Get Auth Context Data
-    const { createUser, updateUserProfile } = useContext(AuthContext);
+    const { createUser, socialLogin, updateUserProfile } = useContext(AuthContext);
+
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const from = location.state?.from?.pathname || '/';
+
+    // Google Provider Instance
+    const googleProvider = new GoogleAuthProvider();
+    // Facebook Provider Instance
+    const facebookProvider = new FacebookAuthProvider();
 
     // Handle Register Form Data 
     const handleRegistration = e => {
@@ -27,6 +39,7 @@ const Register = () => {
                 form.reset();
                 handleProfileUpdate(name, photo);
                 toast.success('Registration successful! Please verify your email address.');
+                navigate(from, { replace: true });
             })
             .catch(err => {
                 console.error(err);
@@ -43,6 +56,30 @@ const Register = () => {
         updateUserProfile(profile)
             .then(() => { })
             .catch(err => console.error(err));
+    }
+
+    // Handle Google Login
+    const handleGoogleLogIn = () => {
+        socialLogin(googleProvider)
+            .then(result => {
+                const user = result.user;
+                // console.log(user);
+                setError('');
+                navigate(from, { replace: true });
+            })
+            .catch(err => console.error(err))
+    }
+
+    // Handle Facebook Login 
+    const handleFacebookLogin = () => {
+        socialLogin(facebookProvider)
+            .then(result => {
+                const user = result.user;
+                // console.log(user);
+                setError('');
+                navigate(from, { replace: true });
+            })
+            .catch(err => console.error(err))
     }
 
     return (
@@ -83,8 +120,8 @@ const Register = () => {
                                     </div>
                                     <hr />
                                     <div className="text-center social-login">
-                                        <Link className="d-block btn btn-outline-success"><FaGoogle /> Signup with Google</Link>
-                                        <Link className="d-block btn btn-outline-success mt-2"><FaFacebook /> Signup with Facebook</Link>
+                                        <Link onClick={handleGoogleLogIn} className="d-block btn btn-outline-success"><FaGoogle /> Signup with Google</Link>
+                                        <Link onClick={handleFacebookLogin} className="d-block btn btn-outline-success mt-2"><FaFacebook /> Signup with Facebook</Link>
                                     </div>
                                 </div>
                             </form>
