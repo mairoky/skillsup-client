06edd45b49fdd9d60a6624f6a74f28c2704bb1a8
Aminul Islam Rocky <mairoky@outlook.com>
2022-10-27 19:16:58 +0600

    404 page added

--- src/pages/PageNotFound/PageNotFound.js
+++ src/pages/PageNotFound/PageNotFound.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Image from '../../images/pagenotfound.png';
 
 const PageNotFound = () => {
     return (
-        <div>
-            <h3>Page Not Found</h3>
+        <div className='py-5'>
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-lg-6 col-md-10">
+                        <div className="text-center">
+                            <img className='img-fluid' src={Image} alt="" />
+                            <p>The page you are looking for does not exist!!!</p>
+                            <Link to="/" className='btn btn-outline-danger'>Back to Home</Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 };
