acff92381e1f10e8d2d382388f3c98b7fe9f6b56
Aminul Islam Rocky <mairoky@outlook.com>
2022-10-27 20:36:31 +0600

    add env for firebase key

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDkNHM8VSF6j9Oqjivc3EbTXW2Tdnb4eOs",
-    authDomain: "learning-website-react.firebaseapp.com",
-    projectId: "learning-website-react",
-    storageBucket: "learning-website-react.appspot.com",
-    messagingSenderId: "139991343661",
-    appId: "1:139991343661:web:811c2af5921397bd7b0ea4"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
